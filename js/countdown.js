ca18daf16824c43c1abca949fa668ef28813e6e4
xiandsve <161516043+xiandsve@users.noreply.github.com>
2024-07-30 00:23:10 +0300

    Final touches (#33)
    * final touches
    
    * don't dispay countdown when the wedding has passed
    
    ---------
    
    Co-authored-by: sve <>

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the target date
-const targetDate = new Date("Sep 22, 2024 10:00:00").getTime();
+const targetDate = new Date("Sep 22, 2024 10:15:00").getTime();
 
 function padNumber(number) {
     return number.toString().padStart(2, '0');
@@ -8,10 +8,7 @@ function padNumber(number) {
 
 function updateCountdown() {
     const now = new Date().getTime();
-    // console.log(targetDate);
-    // console.log(now);
     const distance = Math.max(targetDate - now, 0);
-
 
     // Calculate days, hours, minutes, and seconds
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -19,14 +16,12 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result
-    document.getElementById("countdown").innerHTML = 
-        padNumber(days) + " дни " + padNumber(hours) + " часа " + padNumber(minutes) + " минути " + padNumber(seconds) + " секунди";
-
-    // If the countdown is over, display some text
-    if (distance < 0) {
-        clearInterval(interval);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
+    if (distance > 0) {
+        document.getElementById("countdown").innerHTML = 
+        padNumber(days) + " дни " 
+        + padNumber(hours) + " часа " 
+        + padNumber(minutes) + " минути " 
+        + padNumber(seconds) + " секунди";
     }
 }
 
